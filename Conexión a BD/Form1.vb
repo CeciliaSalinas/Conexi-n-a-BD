df3d01c753ab2fa6ae2afc15b389026f3d8d210e
cecilialujansalinas@outlook.com <cecilialujansalinas@outlook.com>
2023-10-12 15:52:01 -0300

    Agregar archivos de proyecto.

---
+++ Conexión a BD/Form1.vb
@@ -0,0 +1,15 @@
+﻿'utilizamos el espacio de nombre y poder utilizar esos objetos del espacio de nombres
+Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+
+        'Abrir conexión declarando una nueva clase publica de tipo clselibreriasql en el archivo útiles
+        Try
+
+            MsgBox("Conexion exitosa")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
